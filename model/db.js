c6c28e7506b154625b3d0591eef95033dda97e2a
Hassan Butt <115375913+HassanButt007@users.noreply.github.com>
2023-10-08 14:12:38 +0500

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    "mongodb+srv://hassan:hassan@cluster0.prqhxtw.mongodb.net/crudOp?retryWrites=true&w=majority", { useNewUrlParser: true}
+    //Wrong DB Link
+    "mongodb+srv://hassan:fffaa@cluster0.prqhxtw.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true}
     , (err) => {
         if (!err){console.log("connection succeeded ")}
         else{console.log("connection poor : " + err)};
